80b155200b5a1887416d244911c7d0d0198c3887
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-04-24 21:22:08 -0300

    Settings controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,10 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from './repositories/SettingsRepository';
+import { SettingsController } from './controllers/SettingsController';
 
 const routes = Router();
 
-routes.post('/settings', async (request, response) => {
-  const { chat, username } = request.body;
-  const settingsRepositoy = getCustomRepository(SettingsRepository);
-  const settings = settingsRepositoy.create({
-    chat,
-    username,
-  });
+const settingsController = new SettingsController();
 
-  await settingsRepositoy.save(settings);
-
-  return response.json(settings);
-});
+routes.post('/settings', settingsController.create);
 
 export { routes };
