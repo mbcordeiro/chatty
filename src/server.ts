8831a10ad210df0e0f573d5474fabdd69aad2bda
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-04-28 00:30:42 -0300

    Socket io

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 import { routes } from './routes';
 
@@ -6,8 +8,16 @@ import './database';
 
 const app = express();
 
+const http = createServer(app);
+
+const io = new Server(http);
+
+io.on('connection', (socket: Socket) => {
+  console.log('Connected => ', socket.id);
+});
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+http.listen(3333, () => console.log('Server is running on port 3333'));
