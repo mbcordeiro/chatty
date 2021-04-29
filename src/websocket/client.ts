db1cf24200816bb57065770df740ffc1109533de
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-04-28 22:26:44 -0300

    Client socket create user and create connection

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,8 +1,38 @@
 import { Socket } from 'socket.io';
 import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { UsersService } from '../services/UsersService';
+
+const connectionsService = new ConnectionsService();
+const usersService = new UsersService();
 
 io.on('connection', (socket: Socket) => {
-  socket.on('client_first_access', params => {
-    console.log(params);
+  socket.on('client_first_access', async params => {
+    const socket_id = socket.id;
+    const { text, email } = params;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create(email);
+
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id,
+      });
+    } else {
+      const connection = await connectionsService.findByUserId(userExists.id);
+
+      if (!connection) {
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id,
+        });
+      } else {
+        connection.socket_id = socket_id;
+
+        await connectionsService.create(connection);
+      }
+    }
   });
 });
