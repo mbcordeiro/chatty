ccb0b4201ab30d65dc46d77197f21dfa34ca7355
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-05-08 22:03:40 -0300

    Admin send to client

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,7 @@
+let socket = null;
+
 document.querySelector('#start_chat').addEventListener('click', event => {
-  const socket = io();
+  socket = io();
 
   const chat_help = document.getElementById('chat_help');
   chat_help.style.display = 'none';
@@ -42,5 +44,17 @@ document.querySelector('#start_chat').addEventListener('click', event => {
         document.getElementById('messages').innerHTML += rendered;
       }
     });
+  });
+
+  socket.on('admin_send_to_client', message => {
+    socket_admin_id = message.socket_id;
+
+    const template_admin = document.getElementById('admin-template').innerHTML;
+
+    const rendered = Mustache.render(template_admin, {
+      message_admin: message.text,
+    });
+
+    document.getElementById('messages').innerHTML += rendered;
   });
 });
